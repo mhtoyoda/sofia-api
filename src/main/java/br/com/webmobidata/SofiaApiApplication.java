8ebab018026ebc0be64d98f6d99883de4684af42
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-04-29 19:38:38 -0300

    Estrutura inicial

---
+++ src/main/java/br/com/webmobidata/SofiaApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.webmobidata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofiaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofiaApiApplication.class, args);
+	}
+}
